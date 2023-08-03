f2dc9ec4500ab7c3df147c1e33f40ae1aa21b35c
razuAhmad <abdurrahmanrazu2000@gmail.com>
2023-08-03 18:07:59 +0600

    ready for work

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import '@styles/global.css';
+
+import Nav from '@components/Nav';
+
+
+export const metadata= {
+    title: "nextjsdemoapp",
+    description: "Discover & share AI Prompts"
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+        <body>
+         
+            <div className='main'>
+                <div className="gradient"/>
+            </div>
+
+            <main className='app'>
+                <Nav/>
+                {children}
+            </main>
+            
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
